82b40ff4d6dc65bf6c47907ac61b4cdd0ac86271
dinithih <dinithiliyanage.95@gmail.com>
2023-04-09 21:03:47 +0530

    10:space rest service

--- mrating-rest-service/src/main/java/com/example/demo/controller/SpaceRestController.java
+++ mrating-rest-service/src/main/java/com/example/demo/controller/SpaceRestController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.commons.CommonResponse;
 import com.example.demo.entity.Space;
-import com.example.demo.entity.User;
-import com.example.demo.exceptions.APIException;
 import com.example.demo.services.SpaceServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +32,16 @@ public class SpaceRestController {
     @PostMapping("/space")
     public ResponseEntity<Space> addSpace(@RequestBody Space space)  {
         return ResponseEntity.ok(spaceServices.addSpace(space));
+    }
+
+    @GetMapping("/space/{space_id}")
+    public ResponseEntity<Space> getSpace(@PathVariable String space_id){
+        return ResponseEntity.ok(spaceServices.getSpace(space_id));
+    }
+
+    @DeleteMapping("/space/{space_id}")
+    public ResponseEntity<CommonResponse> deleteSpace(@PathVariable String space_id){
+        spaceServices.deleteSpace(space_id);
+        return ResponseEntity.ok(new CommonResponse("Successful", "Space deletion successful"));
     }
 }
