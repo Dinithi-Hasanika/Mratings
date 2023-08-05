264319aeb971b20f92b7ed3ec5d6003ea2f01d7c
dinithih <dinithiliyanage.95@gmail.com>
2023-08-05 12:41:52 +0530

    2. mrater home changes

---
+++ mratings/src/app/ui/mrater-view/mrater-spaces/mrater-spaces.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-mrater-spaces',
+  templateUrl: './mrater-spaces.component.html',
+  styleUrls: ['./mrater-spaces.component.css']
+})
+export class MraterSpacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
