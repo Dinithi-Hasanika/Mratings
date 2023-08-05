5542ba71423ba641257e0c7d6c2970be96f75fa0
dinithih <dinithiliyanage.95@gmail.com>
2023-08-05 16:04:42 +0530

    space card for Inputs

--- mratings/src/app/ui/mrater-view/space-card/space-card.component.ts
+++ mratings/src/app/ui/mrater-view/space-card/space-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-space-card',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class SpaceCardComponent implements OnInit {
 
   constructor() { }
+
+@Input() spaceName= '';
+@Input() description= '';
+@Input() country= '';
+@Input() movies= 0;
+@Input() subscribers= 0;
 
   ngOnInit(): void {
   }
