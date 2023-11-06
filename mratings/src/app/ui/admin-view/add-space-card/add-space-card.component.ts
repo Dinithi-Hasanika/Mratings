788d443059156614f3ea10df101794e0f6112186
dinithih <dinithiliyanage.95@gmail.com>
2023-11-06 14:57:51 +0530

    add spaces and space cards

---
+++ mratings/src/app/ui/admin-view/add-space-card/add-space-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-space-card',
+  templateUrl: './add-space-card.component.html',
+  styleUrls: ['./add-space-card.component.css']
+})
+export class AddSpaceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ mratings/src/app/ui/admin-view/add-spaces/add-spaces.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-spaces',
+  templateUrl: './add-spaces.component.html',
+  styleUrls: ['./add-spaces.component.css']
+})
+export class AddSpacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
