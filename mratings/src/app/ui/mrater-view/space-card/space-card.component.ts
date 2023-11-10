adffd1a17b885199d3f606cea153596f29e3ad77
dinithih <dinithiliyanage.95@gmail.com>
2023-11-10 12:34:36 +0530

    TF0007 - UnSubscribe movie space from my spaces

--- mratings/src/app/ui/mrater-view/space-card/space-card.component.ts
+++ mratings/src/app/ui/mrater-view/space-card/space-card.component.ts
@@ -16,6 +16,8 @@ export class SpaceCardComponent implements OnInit {
 @Input() space = {} as Space;
 @Input() isAdmin = false;
 @Input() appUser = new User("","","","",[]) ;
+@Input() isMySpace = false;
+
 isEdit: boolean = false;
 userSpaces: Space[];
 
@@ -24,9 +26,7 @@ userSpaces: Space[];
 
   deleteSpace(spaceId: any){
     console.log(spaceId);
-    this.spaceService.deleteSpace(spaceId).subscribe(data => {
-console.log(data);
-    });
+    this.spaceService.deleteSpace(spaceId).subscribe(data => {});
     location.reload();
   }
 
@@ -47,12 +47,22 @@ console.log(data);
   }
 
   subscribeToSpace(){
-   this.addSpacetoUser();
-   this.increaseSpaceSubscribers();
+   this.updateUserSpaces("subscribe");
+   this.updateSpaceSubscribers("subscribe");
   }
 
-  increaseSpaceSubscribers() {
-    this.space.followers = this.space.followers + 1;
+  unsubscribeSpace(){
+    this.updateUserSpaces("unsubscribe");
+    this.updateSpaceSubscribers("unsubscribe");
+  }
+
+  updateSpaceSubscribers(option: string) {
+    if(option === "subscribe"){
+      this.space.followers = this.space.followers + 1;
+    }else if(option === "unsubscribe"){
+      this.space.followers = this.space.followers - 1;
+    }
+    console.log(this.space.followers);
     this.spaceService.editSpace(this.space).subscribe(data => {
      console.log("space "+data);
      if(data){
@@ -61,14 +71,23 @@ console.log(data);
    });
   }
 
-  addSpacetoUser(){
-    console.log("im in");
-if(!this.appUser.userSpaces.includes(this.space)){
-  this.appUser.userSpaces.push(this.space);
-}
-console.log(this.appUser);
-this.userService.addUser(this.appUser).subscribe(data => {
-  console.log(data);
-});
+  updateUserSpaces(option: string){
+    if(option === "subscribe"){
+      if(!this.appUser.userSpaces.includes(this.space)){
+        this.appUser.userSpaces.push(this.space);
+      }
+      console.log(this.appUser);
+    } else if(option === "unsubscribe"){
+      if(this.appUser.userSpaces.includes(this.space)){
+        var index = this.appUser.userSpaces.indexOf(this.space);
+        if(index != -1){
+          this.appUser.userSpaces.splice(index,1);
+        }
+      }
+      console.log(this.appUser);
+    }
+    this.userService.addUser(this.appUser).subscribe(data => {
+      console.log(data);
+    });
   }
 }
